80b91a0eff3043ceed51d92642718c6b829a23c9
Setu Kumar Basak <sbasak4@ncsu.edu>
2021-08-28 13:37:38 -0400

    Added setup.py initial file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='glare',
+    version='1.0',
+    packages=['glare'],
+    license='MIT',
+    author='Setu Kumar Basak',
+    author_email='sbasak4@ncsu.edu',
+    description='Module for sample homework setup'
+)
